ffd14a5dfbb95d6cfc915fb1256d573269aa9583
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-04-21 18:21:25 +0700

    Initial commit

---
+++ SK-Lib-cstdio-vfscanf-Bagian3__CPP/Source.cpp
@@ -0,0 +1,40 @@
+#pragma warning(disable:4996)
+
+#include <iostream>
+#include <cstdio>
+#include <cstdarg>
+#include <stdexcept>
+#include <conio.h>
+
+/*
+    Source by CPPReference
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+void periksa_sscanf(int count, const char* buf, const char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    if (std::vsscanf(buf, fmt, ap) != count) {
+        throw std::runtime_error("parsing error");
+    }
+
+    va_end(ap);
+}
+
+int main() {
+    try {
+        int n, m;
+        std::cout << "Parsing '1 2'...";
+        periksa_sscanf(2, "1 2", "%d %d", &n, &m);
+        std::cout << "success\n";
+        std::cout << "Parsing '1 a'...";
+        periksa_sscanf(2, "1 a", "%d %d", &n, &m);
+        std::cout << "success\n";
+    } catch (const std::exception& e) {
+        std::cout << e.what() << '\n';
+    }
+
+    _getch();
+    return 0;
+}
